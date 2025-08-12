2efb2d55b2387b881e91f94eeeb8e3bbcc230f79
Viacheslav Filipenko <filipenko.slavik@gmail.com>
2025-08-12 23:21:59 +0200

    fix: mime type issue

--- src/files/file.controller.ts
+++ src/files/file.controller.ts
@@ -4,13 +4,15 @@ import {
     Post,
     UploadedFile,
     UseInterceptors,
-    StreamableFile, Param, Res,
+    StreamableFile,
+    Param,
 } from '@nestjs/common';
 import { memoryStorage } from 'multer';
 import {FileInterceptor, } from "@nestjs/platform-express";
 import { createReadStream, createWriteStream } from 'fs';
 import { join } from 'path';
 import { v4 } from 'uuid';
+import { contentType, lookup, extension } from 'mime-types';
 
 @Controller('files')
 export class FileController {
@@ -20,11 +22,11 @@ export class FileController {
     getFile(@Param('fileName') fileName: string): StreamableFile {
         const filePath = join(process.cwd(), 'uploads', fileName);
         const file = createReadStream(filePath);
+        const mimetype = contentType(fileName) || 'application/octet-stream';
 
         return new StreamableFile(file, {
-            type: `image/png`,
+            type: mimetype,
             disposition: 'attachment',
-            length: undefined, // You can set the length if known
         });
     }
 
@@ -32,18 +34,17 @@ export class FileController {
     @UseInterceptors(
         FileInterceptor('file', {
             storage: memoryStorage(),
-            limits: { fileSize: 20 * 1024 * 1024 },
         }),
     )
     uploadFile(@UploadedFile() file: Express.Multer.File): string {
-        const { mimetype } = file;
-        const fileExtension = mimetype.split('/')[1];
-        const uuid = v4();
-        const filePath = join(process.cwd(), 'uploads', `${uuid}.${fileExtension}`);
+        const mimeType = lookup(file.originalname) || 'application/octet-stream';
+        const ext = extension(mimeType) || 'bin';
+        const fileName = `${v4()}.${ext}`;
+        const filePath = join(process.cwd(), 'uploads',fileName);
         const fileStream = createWriteStream(filePath);
 
         fileStream.write(file.buffer);
 
-        return `http://localhost:3000/files/${uuid}.${fileExtension}`;
+        return `http://localhost:3000/files/${fileName}`;
     }
 }
